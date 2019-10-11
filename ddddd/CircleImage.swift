cbffbfaa72f29c68e4dbb2f06adf1a7f2a194027
leeteukki <tklohj@hanyang.ac.kr>
2019-10-12 01:01:10 +0900

    First SwiftUI_Example

--- ddddd/CircleImage.swift
+++ ddddd/CircleImage.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019 lee chat. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("koreaFlag")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+        
+    }
+}
+
+struct CircleImage_Preview: PreviewProvider {
+    static var previews: some View{
+        CircleImage()
+    }
+}
